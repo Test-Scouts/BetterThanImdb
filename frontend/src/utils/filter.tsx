41c2c18b1ef9dc0beeebdb276228efc6b25df04d
Mert Yurdakul <mrtyrdkl4@gmail.com>
2025-08-17 22:30:01 +0200

    Add: Movie interface and filtering functions created.

--- frontend/src/utils/filter.tsx
+++ frontend/src/utils/filter.tsx
@@ -1,3 +1,9 @@
-export function filterMovies(movies: Movie[], search: string): Movie[] {
-  return movies.filter((movie) => movie.title.toLowerCase().includes(search.toLowerCase()));
+import { Movie } from "../types/Movie";
+
+export function filterMoviesByName(movies: Movie[], keyword: string): Movie[] {
+  return movies.filter((movie) => movie.movieName.toLowerCase().includes(keyword.toLowerCase()));
+}
+
+export function filterMoviesByRating(movies: Movie[], minRating: number): Movie[] {
+  return movies.filter((movie) => movie.movieRating >= minRating);
 }
